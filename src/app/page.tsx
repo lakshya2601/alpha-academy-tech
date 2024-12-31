82a4a297049894aaa9786a680f2ed8c1128ac556
Lakshya Porwal <89459982+lakshya2601@users.noreply.github.com>
2024-12-31 18:40:35 +0530

    Integrate Navbar component into RootLayout and remove it from Home page
    - Added Navbar component to the RootLayout for consistent navigation across the application.
    - Removed Navbar import and usage from the Home page to streamline the layout.
    
    These changes improve the overall structure and maintainability of the application.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar";
 import HomePage from "../components/Home/page";
 import Home2 from "../components/Home/home2";
 import Home3 from "@/components/Home/home3";
@@ -7,7 +6,6 @@ import Home4 from "@/components/Home/home4";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen">
-      <Navbar />
       <HomePage />
       <Home2 />
       <Home3 />
