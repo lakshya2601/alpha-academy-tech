90d4e158d3074d9e1cc515987c633630599ca23c
Lakshya Porwal <89459982+lakshya2601@users.noreply.github.com>
2025-04-10 12:57:54 +0530

    svg updates

--- src/components/Home/home3.tsx
+++ src/components/Home/home3.tsx
@@ -5,16 +5,14 @@ const Home3 = () => {
   return (
     <div className="w-full h-full md:h-[100vh] flex flex-col items-center justify-center">
       <div className="flex flex-col md:flex-row w-full h-auto md:h-[200px] justify-between px-6 md:px-[80px] mt-6 md:mt-[80px] mb-6 md:mb-[50px] items-center text-center md:text-left">
-        <h1 className="text-3xl md:text-5xl font-serifRegular text-foreground mb-4 md:mb-0">
-          One More main <br className="block md:hidden" />
-          text here
-        </h1>
+      <h1 className="text-3xl md:text-5xl font-serifRegular text-foreground mb-4 md:mb-0 leading-snug">
+  Empowering Your <br />
+  <span className="text-foreground">Financial Future</span>
+</h1>
+
 
         <p className="w-full md:w-2/3 text-base border-foreground border-t-2 border-b-2 border-r-0 border-l-0 py-4">
-          Since our inception, we have been successful in spotting several great
-          investment opportunities early and have managed to generate
-          sustainable wealth for our clients. Our team enjoys doing this day in
-          and day out and we believe this is our &quot;Dharma&quot;.
+        At Alpha Academy, we are driven by a commitment to help you grow and protect your wealth. From identifying promising investment opportunities to building robust financial strategies, we bring deep expertise and data-driven insights to the table. Our purpose is to guide you at every step of your financial journey — because your goals are our mission.
         </p>
       </div>
       <CardComponent />
